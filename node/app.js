2b9a751eb3e62c7bade60ff18b03022fa4bc1cd4
AhnJG <arki13@naver.com>
2019-07-02 18:31:50 +0000

    g

--- node/app.js
+++ node/app.js
@@ -13,3 +13,21 @@ app.get('/', function(req, res) {
 app.get('/main', function(req, res) {
 	res.sendFile(__dirname + "/public/main.html");
 });
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine', 'ejs')
+
+app.post('/email_post', function(req, res){
+	var name = req.body.email;
+	//res.send("<h1> Welcome! </h1> " + name);
+	res.render("email.ejs", {name: req.body.email})
+});
+
+app.post('/ajax_send_email', function(req, res){
+	console.log(req.body.email)
+	
+	var responseData = {'result': 'ok', 'email': req.body.email}
+	res.json(responseData)
+});
