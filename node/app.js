4e45829274302593071d370f5f1c04fed5128e07
JungGeun Ahn <arki13@naver.com>
2019-07-03 16:01:10 +0900

    Update app.js

--- node/app.js
+++ node/app.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
 	host : 'localhost',
 	port : 3306,
 	user : 'root',
-	password : 'qkeksnsk12',
+	password : '',
 	database : 'testnode'})
 
 connection.connect()
